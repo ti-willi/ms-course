5061713e4eff8b931661f75d13960268ae27f233
Willian Comiotto <willianbcomiotto@gmail.com>
2024-03-21 17:17:09 -0300

    Configuring Zuul api gateway

---
+++ hr-api-gateway-zuul/src/test/java/com/tiwilli/hrapigatewayzuul/HrEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tiwilli.hrapigatewayzuul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
