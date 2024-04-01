d5969329ba72100f92fae3b8e71092d9250949ef
Willian Comiotto <willianbcomiotto@gmail.com>
2024-04-01 16:26:56 -0300

    db config server

--- hr-worker/src/main/java/com/tiwilli/hrworker/resources/WorkerResource.java
+++ hr-worker/src/main/java/com/tiwilli/hrworker/resources/WorkerResource.java
@@ -23,9 +23,6 @@ public class WorkerResource {
 
     private static Logger logger = LoggerFactory.getLogger(WorkerResource.class);
 
-    @Value("${test.config}")
-    private String testConfig;
-
     @Autowired
     private Environment environment;
 
@@ -34,7 +31,7 @@ public class WorkerResource {
 
     @GetMapping(value = "/configs")
     public ResponseEntity<Void> getConfigs() {
-        logger.info("CONFIG = " + testConfig);
+        //logger.info("CONFIG = " + testConfig);
         return ResponseEntity.noContent().build();
     }
 
